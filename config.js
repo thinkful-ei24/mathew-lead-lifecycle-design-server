aa20241eaab50320be564fefd40c2845fcb78595
Matt Williamson <mathewbwilliamson@gmail.com>
2018-10-25 15:49:37 -0400

    Finished back end testing for now

--- config.js
+++ config.js
@@ -10,8 +10,6 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_LEAD_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
-  PRODUCTION_DATABASE_URL: 
-        process.env.PROD_LEAD_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
