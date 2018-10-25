4f918df11ef5c5ad7b547f3bca2624bac671e682
Matt Williamson <mathewbwilliamson@gmail.com>
2018-10-25 17:37:31 -0400

    Fixed a bug with the seed database

--- config.js
+++ config.js
@@ -10,6 +10,8 @@ module.exports = {
   TEST_DATABASE_URL:
         process.env.TEST_LEAD_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test',
+  PRODUCTION_DATABASE_URL:
+        process.env.PRODUCTION_LEAD_DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
